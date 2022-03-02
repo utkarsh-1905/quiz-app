92707c443df9508234b6fb4c0b61cba50cddb3b8
destrex271 <destrex271@gmail.com>
2022-03-02 23:16:52 +0530

    Le bhai thik frontend

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   document.body.style = "background-color: #0F0F0F";
   return (
     <React.StrictMode>
-      <Title name="Quiz" />
+      <Title title="Quiz" />
       <div className="row mt-5">
         <div className="col-lg-3 col-xl-3 col-md-3 col-sm-3 col-xs-1"></div>
         <div className="col-lg-6 col-xl-6 col-md-6 col-sm-6 col-xs-10 p-5">
